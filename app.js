f458de0a7297abd838ca84c78621fc953978e120
Raj <raj25oct2003@gmail.com>
2025-12-24 12:38:54 +0530

    Fix CORS preflight for Netlify frontend

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
+app.options("*", cors());
 import loanRoutes from "./Routes/loan.routes.js";
 import adminRoutes from "./Routes/admin.routes.js";
 
