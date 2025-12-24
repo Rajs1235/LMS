08c62e6718797e331d2a0ac48b466617a9bfa2e4
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-12-24 17:50:09 +0530

    Remove redundant CORS options setup

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
-app.options("*", cors());
+ 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
